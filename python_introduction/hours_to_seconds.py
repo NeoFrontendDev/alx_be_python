370434fb93176c2e1338a2877543a6ed891e3512
NeoMosebi <neosammosebi01@gmail.com>
2025-11-08 12:05:42 +0200

    hours to seconds calculator created and executed

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,6 @@
 Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
 >>> hours = 2
->>> seconds = 2 * 3600
+>>> seconds = hours * 3600
 >>> print(f"{hours} hour(s) is {seconds} seconds.")
 2 hour(s) is 7200 seconds.
