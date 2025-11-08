03357273f65948dc1a1dbce788bf080f78a09aca
NeoMosebi <neosammosebi01@gmail.com>
2025-11-08 12:24:23 +0200

    future age calculator created and executed

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> current_age = int(input("How old are you? "))
+How old are you? 30
+>>> future_age = current_age + 27
+>>> print(f"In 2050, you will be {future_age} years old.")
+In 2050, you will be 57 years old.
