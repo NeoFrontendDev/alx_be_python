ecd753fd1adcadffb791f67490a2dc9ac9fb1b63
NeoMosebi <neosammosebi01@gmail.com>
2025-11-08 11:42:01 +0200

    area calculator created and executed

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,7 +1,7 @@
 Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
 >>> length = 10
->>> width =5
->>> Area = length * width
->>> print(f"The area of the rectangle is: {Area}")
+>>> width = 5
+>>> area = length * width
+>>> print(f"The area of the rectangle is: {area}")
 The area of the rectangle is: 50
