4ef61013e09a4a564af5cecfe27f927aa7b68765
NeoMosebi <neosammosebi01@gmail.com>
2025-11-08 11:31:34 +0200

    interest calculator created and executed

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> principal = 1000
+>>> rate = 0.05
+>>> time = 3
+>>> interest = principal * rate * time
+>>> print(f"The simple interest is: {interest}")
+The simple interest is: 150.0
