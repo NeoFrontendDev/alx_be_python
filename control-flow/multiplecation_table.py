f207cac6990a14cc7a91d794fcbba3118cb06458
NeoMosebi <neosammosebi01@gmail.com>
2025-11-16 21:42:10 +0200

    multiplecation table created

---
+++ control-flow/multiplecation_table.py
@@ -0,0 +1,7 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> number = int(input("Enter a number to see its multiplication table: "))
+Enter a number to see its multiplication table: 5
+>>> for i in range(1, 11):
+...     result = number * 1
+...     print(f"{number} * {i} = {result}")
