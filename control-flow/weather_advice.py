374f79b87cbefebe1a18fc309a4ed72806b78d4b
NeoMosebi <neosammosebi01@gmail.com>
2025-11-17 11:21:02 +0200

    weather advice created and excecuted

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,7 @@
 Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
 >>> weather = input("What's the weather like today? (sunny/rainy/cold): ")
-What's the weather like today? (sunny/rainy/cold): rainy
+What's the weather like today? (sunny/rainy/cold): sunny
 >>> 
 >>> if weather == "sunny":
 ...     print("Wear a t-shirt and sunglasses.")
@@ -10,3 +10,7 @@ What's the weather like today? (sunny/rainy/cold): rainy
 ... elif weather == "cold":
 ...     print("Make sure to wear a warm coat and a scarf.")
 ... else:
+...     print("Sorry, I don't have recommendations ffor this weather.")
+... 
+...     
+Wear a t-shirt and sunglasses.
