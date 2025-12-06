24219b6c2a04f12fa1e3408a49265a47de4fe91a
NeoMosebi <neosammosebi01@gmail.com>
2025-12-07 00:21:03 +0200

    updated

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,3 @@
-Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 >>> class BankAccount:
 ...     def __init__(self, initial_balance=0):
 ...         """Initialize account with an optional starting balance."""
